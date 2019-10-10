38f632f83579ef288a3b09bde7c3d768583e1a9d
TheTerabit <terrabitroman@gmail.com>
2019-10-10 17:02:59 +0200

    Init

---
+++ src/test/java/pl/bs/RoomBookerDDD/RoomBookerDddApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.bs.RoomBookerDDD;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RoomBookerDddApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
